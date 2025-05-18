74b65842e798d908541d14d4155960e979d94303
Alwaysprafull <Prafullmishra089@gmail.com>
2025-05-18 22:56:13 +0530

    17th may 2025

---
+++ ex_08_If_Condition/Lab082_If_Else_Scanner.java
@@ -0,0 +1,10 @@
+package ex_08_If_Condition;
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+public class Lab082_If_Else_Scanner {
+
+    public static void main(String[] args) {
+
+
+    }
+}
