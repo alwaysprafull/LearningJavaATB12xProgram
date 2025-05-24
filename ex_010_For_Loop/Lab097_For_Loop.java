c630ad989bd8477c968e680560df9855ab7eb730
Alwaysprafull <Prafullmishra089@gmail.com>
2025-05-24 11:31:19 +0530

    24th may 2025 P1

---
+++ ex_010_For_Loop/Lab097_For_Loop.java
@@ -0,0 +1,4 @@
+package ex_010_For_Loop;
+
+public class Lab097_For_Loop {
+}
---
+++ ex_010_For_Loop/Lab099_For_Loop_Var_Name.java
@@ -0,0 +1,11 @@
+package ex_010_For_Loop;
+
+public class Lab099_For_Loop_Var_Name {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
