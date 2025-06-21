3d8832a80d36741908d1aa050661613463cb0268
Alwaysprafull <Prafullmishra089@gmail.com>
2025-06-21 11:54:36 +0530

    Coding Challenge 3

---
+++ CodingTest/CodingChallenge_3.java
@@ -0,0 +1,21 @@
+package CodingTest;
+
+public class CodingChallenge_3 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a == b: " + (a == b));   // false
+        System.out.println("a != b: " + (a != b));   // true
+        System.out.println("a > b: " + (a > b));     // true
+        System.out.println("a < b: " + (a < b));     // false
+        System.out.println("a >= b: " + (a >= b));   // true
+        System.out.println("a <= b: " + (a <= b));   // false
+
+        System.out.println("(a > b) && (a > 0): " + ((a > b) && (a > 0)));  // true
+        System.out.println("(a < b) || (a > 0): " + ((a < b) || (a > 0)));  // true
+        System.out.println("!(a > b): " + (!(a > b)));                      // false
+    }
+}
+
